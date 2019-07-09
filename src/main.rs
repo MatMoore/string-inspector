5b7b25481a7996ed7c527c627bb19d023634b207
Mat Moore <matmoore@users.noreply.github.com>
2019-07-09 13:02:24 +0100

    Always align characters to bytes
    Show ascii escape characters rather than rendering control characters directly,
    and similarly show unicode codepoints for anything outside of ascii.
    
    The second decision is not ideal, because if there are printable non ascii
    characters you should be able to see what they are. But this allows me to
    postpone dealing with a lot of complexity around how fonts render unicode, for example:
      - whitespace and halfwidth vs fullwidth characters will complicate my attempts to align bytes and characters
      - combining characters or RTL markers will mess up output that comes after it
      - where a grapheme cluster is made up of multiple printable characters, showing individual characters might not be the most useful thing to display

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,37 @@ fn main() {
     //let mut buffer = String::new();
     //io::stdin().read_to_string(&mut buffer).expect("Unable to read from stdin");
     //println!("{}", buffer);
-    let buffer = "hello world, here is £1";
+    let buffer = "hello world, here is\t£1";
     let mut byte_buffer = [0; 4];
 
-    for character in buffer.chars() {
-        let char_size = character.encode_utf8(&mut byte_buffer).len();
-        print!("{:width$}", character, width = char_size * 3);
+    // First print the raw bytes
+    // this should be unambiguous and each one always takes up a fixed width of 3
+    for byte in buffer.bytes() {
+        print!("{:02x} ", byte)
     }
     println!("");
 
-    for byte in buffer.bytes() {
-        print!("{:x} ", byte)
+    // Now the characters. This assumes a single character cannot be more
+    // than 3 "characters" wide, which breaks down if we print control characters directly.
+    // So we should print escape characters for these.
+    // Ascii escapes can fit within 3 chars e.g. "\t " but unicode escapes cannot e.g. "u+feff BOM"
+    for character in buffer.chars() {
+        let char_size = character.encode_utf8(&mut byte_buffer).len();
+
+        match character {
+            '\t' | '\r' | '\n' => {
+                print!("{:width$} ", character.escape_default(), width = char_size * 3);
+            }
+            '\u{20}'...'\u{7e}' => {
+                print!("{:width$}", character, width = char_size * 3);
+            }
+            _ => {
+                // TODO: this formatting will break if the codepoint in hex is longer than
+                // the byte representation in hex
+                let codepoint = format!("{:x}", character as u32);
+                print!("{:width$}", codepoint, width = char_size * 3);
+            }
+        }
     }
-    println!("")
+    println!("");
 }
